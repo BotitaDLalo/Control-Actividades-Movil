fd777ccc9dce607846b14466bb2d05a8483fd71d
BotitaDLalo <canciondfuego0@gmail.com>
2026-01-11 22:00:31 -0600

    feat(config/network): Agrega interceptor en método para inicializar Dio para mostrar en consola las peticiones http que se realizan.

--- lib/config/network/dio_client.dart
+++ lib/config/network/dio_client.dart
@@ -1,8 +1,21 @@
 import 'package:aprende_mas/config/environment/environment.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 
 final dio = Dio(BaseOptions(
   baseUrl: Environment.apiUrl,
   connectTimeout: const Duration(seconds: 30),
-  validateStatus: (status) => true, // No lanzar excepciones para códigos 4xx/5xx
-));
+  validateStatus: (status) =>
+      true,
+))
+  ..interceptors.add(LogInterceptor(
+    request: true,
+    requestHeader: true,
+    requestBody: true,
+    responseHeader: true,
+    responseBody: true,
+    error: true,
+    logPrint: (obj) {
+      debugPrint(obj.toString());
+    },
+  ));
